b948047d328689f77dbcad0dd74691d557cb0eb0
BS <bohdan.strilets@gmail.com>
2025-08-25 17:51:26 +0200

    Changed the settings for the mine file.

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,36 @@
+import { Logger, ValidationPipe, VersioningType } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import compression from 'compression';
+import cookieParser from 'cookie-parser';
+import helmet from 'helmet';
 
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  const logger = new Logger('Bootstrap');
   const app = await NestFactory.create(AppModule);
+
+  app.setGlobalPrefix('api');
+  app.enableVersioning({ type: VersioningType.URI, defaultVersion: '1' });
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+  app.enableCors({
+    origin: ['http://localhost:3000'],
+    credentials: true,
+  });
+
+  app.use(helmet());
+  app.use(cookieParser('dev-secret'));
+  app.use(compression());
+
   await app.listen(3000);
+
+  logger.debug(`Application listening on port ${3000}`);
 }
 bootstrap();
