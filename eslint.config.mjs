62165977aa3b9ec4ef6adf642d93cbc58867a351
BS <bohdan.strilets@gmail.com>
2025-09-13 19:56:07 +0200

    Add sort imports.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -78,6 +78,25 @@ export default [
     },
     rules: {
       'react/react-in-jsx-scope': 'off',
+      'import/order': [
+        'error',
+        {
+          groups: [
+            'builtin',
+            'external',
+            'internal',
+            'parent',
+            'sibling',
+            'index',
+            'object',
+            'type',
+          ],
+          pathGroups: [{ pattern: '@/**', group: 'internal', position: 'before' }],
+          pathGroupsExcludedImportTypes: ['builtin'],
+          'newlines-between': 'always',
+          alphabetize: { order: 'asc', caseInsensitive: true },
+        },
+      ],
     },
   },
 
