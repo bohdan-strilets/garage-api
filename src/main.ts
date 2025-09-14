edb8f707d723422bfa23bc5453fd406c483b616e
BS <bohdan.strilets@gmail.com>
2025-09-14 04:53:07 +0200

    Add base aplication configuration.

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,38 @@
-import { NestFactory } from '@nestjs/core';
+import { ClassSerializerInterceptor, Logger, ValidationPipe } from '@nestjs/common';
+import { NestFactory, Reflector } from '@nestjs/core';
+import bodyParser from 'body-parser';
+import cookieParser from 'cookie-parser';
+import helmet from 'helmet';
 
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const logger = new Logger('Bootstrap');
+  const reflector = app.get(Reflector);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+
+  app.useGlobalInterceptors(new ClassSerializerInterceptor(reflector));
+
+  app.enableCors({ origin: 'http://localhost:3000', credentials: true });
+  app.use(cookieParser());
+  app.use(helmet());
+
+  app.use(bodyParser.json({ limit: '1mb' }));
+  app.use(bodyParser.urlencoded({ limit: '1mb', extended: true }));
+
+  app.setGlobalPrefix('api');
+
   await app.listen(3000);
+  logger.log('Application is running on: ' + (await app.getUrl()));
 }
+
 bootstrap();
